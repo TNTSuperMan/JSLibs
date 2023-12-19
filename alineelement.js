27fd246ad5acde7eb4a62ad401624d5d68cb0b7e
TNTSuperMan <tntsuperga@gmail.com>
2023-12-19 21:12:55 +0900

    ale

--- alineelement.js
+++ alineelement.js
@@ -1,14 +1,14 @@
-function o(e){
-    return {
-        q: e,
-        attr: function(id,ct){this.q.setAttribute(id,ct);return this},
-        html: function(ct){this.q.innerHTML=ct;return this},
-        text: function(ct){this.q.innerText=ct;return this},
-        apde: function(e){this.q.appendChild(e.q);return this},
-        f   : function(f){return this}
-    };
-}
 function $(q,nw){
+    let o=(e)=>{
+        return {
+            q: e,
+            attr: function(id,ct){this.q.setAttribute(id,ct);return this},
+            html: function(ct){this.q.innerHTML=ct;return this},
+            text: function(ct){this.q.innerText=ct;return this},
+            apde: function(e){this.q.appendChild(e.q);return this},
+            f   : function(){return this}
+        };
+    }
     return nw ? o(document.createElement(q)) :
     o(document.querySelector(q))
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 # Javascriptライブラリ作ろう
 創造したライブラリを作りまくります。
+
+[ALineElement](alineelement.js)
+要素操作を一行に。
